8d24ae34487e39edeff04e1155946d7a19570d6d
Adarsh Rai <31541390+adarahrai@users.noreply.github.com>
2020-10-22 09:42:48 +0530

    Update2

--- food_app_ui_animation/lib/pages/StarterPage.dart
+++ food_app_ui_animation/lib/pages/StarterPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app_ui_animation/animations/FadeAnimation.dart';
 
 class StarterPage extends StatefulWidget {
   @override
@@ -33,14 +34,15 @@ class _StarterPageState extends State<StarterPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Text('Taking Order For Delivery', style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),),
+                  FadeAnimation(0.5, Text('Taking Order For Delivery', style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),)),
                   SizedBox(
                     height: 20,
                   ),
-                  Text('See restaurants nearby by \nadding location',style: TextStyle(color: Colors.white, height: 1.4, fontSize: 18),),
+                  FadeAnimation(1,Text('See restaurants nearby by \nadding location',style: TextStyle(color: Colors.white, height: 1.4, fontSize: 18),)),
                   SizedBox(
                     height: 100,
                   ),
+                  FadeAnimation(1.2,
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
@@ -58,11 +60,12 @@ class _StarterPageState extends State<StarterPage> {
                       minWidth: double.infinity,
                       child: Text('Start', style: TextStyle(color: Colors.white),),
                     ),
-                  ),
+                  )),
                   SizedBox(height: 30,),
+                FadeAnimation(1.4,
                   Align(
                     child: Text('Now Deliver To Your 24/7', style: TextStyle(color: Colors.white70, fontSize: 15),),
-                  ),
+                  )),
                   SizedBox(height: 30,)
                 ],
               ),
